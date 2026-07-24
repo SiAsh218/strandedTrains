c4a2786c46034eb5cf90d17f5f756af7e70d390a
SiAsh218 <siashton@live.co.uk>
2026-07-24 13:42:47 +0100

    updated copy to clipboard to include plain text

--- frontend/src/services/tyrellClipboardService.js
+++ frontend/src/services/tyrellClipboardService.js
@@ -9,11 +9,24 @@ const copyToClipboard = async (data) => {
       navigator.clipboard.write &&
       window.ClipboardItem
     ) {
-      const blob = new Blob([html], { type: "text/html" });
+      const htmlBlob = new Blob([html], { type: "text/html" });
+
+      const tempDiv = document.createElement("div");
+      tempDiv.innerHTML = html;
+
+      const plainText = tempDiv.innerText
+        .replace(/\n\s*\n\s*\n+/g, "\n\n")
+        .replace(/[ \t]+\n/g, "\n")
+        .trim();
+
+      const textBlob = new Blob([plainText], {
+        type: "text/plain",
+      });
 
       await navigator.clipboard.write([
         new ClipboardItem({
-          "text/html": blob,
+          "text/html": htmlBlob,
+          "text/plain": textBlob,
         }),
       ]);
     } else {
@@ -56,40 +69,27 @@ const fallbackCopyRichText = (html) => {
 const getDataForCopyToTyrell = (data) => {
   // console.log("Generating data for copy to Tyrell with input:", data);
   return `<b>${data.headcode}</b> is <b>${data.status}</b> at <b>${data.location}</b> ${data.locationW3W ? `(W3W - ${data.locationW3W})` : ""}<br><br>
-  
-  <b>Train Info:</b><br>
-  ${data.strandedAt ? `Stranded At - ${new Date(data.strandedAt).toLocaleString()}<br>` : ""}
-  ${data.traction ? `Traction - ${data.traction}<br>` : ""}
-  <br>
-
-  <b>Passenger Info:</b><br>
-  ${data.passengerCount ? `${data.passengerCount} Passengers<br>` : ""}
-  ${data.passengerLoading ? `${data.passengerLoading} - Passenger Loading<br>` : ""}
-  ${data.vulnerablePeople ? `${data.vulnerablePeople} Vulnerable People<br>` : ""}
-  ${data.noOfStaff ? `${data.noOfStaff} Staff Identified Onboard<br>` : ""}
-  ${data.moodOnboard ? `Mood Onboard - ${data.moodOnboard}<br>` : ""}
-  <br>
-  
-  <b>Facilities:</b><br>
-  ${data.toiletsWorking ? `Toilets - ${data.toiletsWorking}<br>` : ""}
-  ${data.heatingRequired ? `Heating - ${data.heatingRequired}<br>` : ""}
-  ${data.airCoolingRequired ? `Air Cooling - ${data.airCoolingRequired}<br>` : ""}
-  ${data.lighting ? `Lighting - ${data.lighting}<br>` : ""}
-  ${data.paWorking ? `PA System - ${data.paWorking}<br>` : ""}
-  ${data.cateringAvailable ? `Catering - ${data.cateringAvailable}<br>` : ""}
-  <br>
-
-  ${data.tolo ? `<b>TOLO:</b> ${data.tolo}<br><br>` : ""}
-
-  ${
-    data.additionalInformation
-      ? `<b>Additional Information:</b><br>${data.additionalInformation.replace(
-          /\r?\n/g,
-          "<br>",
-        )}<br><br>`
-      : ""
-  }
-  `;
+<b>Train Info:</b><br>
+${data.strandedAt ? `Stranded At - ${new Date(data.strandedAt).toLocaleString()}<br>` : ""}
+${data.traction ? `Traction - ${data.traction}<br>` : ""}
+<br>
+<b>Passenger Info:</b><br>
+${data.passengerCount ? `${data.passengerCount} Passengers<br>` : ""}
+${data.passengerLoading ? `${data.passengerLoading} - Passenger Loading<br>` : ""}
+${data.vulnerablePeople ? `${data.vulnerablePeople} Vulnerable People<br>` : ""}
+${data.noOfStaff ? `${data.noOfStaff} Staff Identified Onboard<br>` : ""}
+${data.moodOnboard ? `Mood Onboard - ${data.moodOnboard}<br>` : ""}
+<br>
+<b>Facilities:</b><br>
+${data.toiletsWorking ? `Toilets - ${data.toiletsWorking}<br>` : ""}
+${data.heatingRequired ? `Heating - ${data.heatingRequired}<br>` : ""}
+${data.airCoolingRequired ? `Air Cooling - ${data.airCoolingRequired}<br>` : ""}
+${data.lighting ? `Lighting - ${data.lighting}<br>` : ""}
+${data.paWorking ? `PA System - ${data.paWorking}<br>` : ""}
+${data.cateringAvailable ? `Catering - ${data.cateringAvailable}<br>` : ""}
+<br>
+${data.tolo ? `<b>TOLO:</b> ${data.tolo}<br><br>` : ""}
+${data.additionalInformation ? `<b>Additional Information:</b><br>${data.additionalInformation.replace(/\r?\n/g, "<br>")}<br><br>` : ""}`;
 };
 
 const tyrellClipboardService = {
